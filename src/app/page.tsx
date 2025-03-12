be658ed6a86c16644c05ca99a52b7497afcd414f
phurichai2547 <phuri.chai@mail.kmutt.ac.th>
2025-03-12 17:30:45 +0700

    theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import About from "./components/Section1/About";
 import WhatWeDo from "./components/Section2/WhatWeDo";
 import Galleries from "./components/Section3/Galleries";
+import GalleriesResponsive from "./components/Section3/GalleriesResponsive";
 import Project from "./components/Section4/Project";
 import Contact from "./components/Section5/Contact";
 import Footer from "./components/Section6/Footer";
@@ -15,6 +16,7 @@ const Page = () => {
       <About />
       <WhatWeDo />
       <Galleries />
+      <GalleriesResponsive />
       <Project />
       <Contact />
       <Footer />
